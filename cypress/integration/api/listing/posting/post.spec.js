76e0903f65a3f4c9e7cd8c0bb409fce435c6aee9
Guilherme da Mata <gcordeiro773@gmail.com>
2025-04-13 19:34:53 -0300

    fix: create spec test to result by keywords

--- cypress/integration/api/listing/posting/post.spec.js
+++ cypress/integration/api/listing/posting/post.spec.js
@@ -1,3 +1,31 @@
+import post from "../../../../mocks/factories/post"
+
 describe('Listing posts', () => {
-    it('Should listing')
+    const posts = post.postData()
+    posts.postContent = 'Essa postagem deve ser encontrada na busca pela paravra chave por alguma palavra'
+
+    beforeEach(() => {
+        cy.createPosting(posts).then(res => {
+            expect(res.body.authorId).to.eq(posts.authorId);
+            expect(res.body.postContent).to.eq(posts.postContent);
+            expect(res.status).to.eq(200);
+        })
+    })
+    afterEach(() => {
+        cy.task("READFROMDB", {
+            dbConfig: Cypress.config('DBAPP'),
+            sql: 'DELETE FROM "Posts" WHERE "UserId" = 4;'
+        }).then(res => {
+            cy.log(res.command)
+            cy.log(res.rowCount)    
+        })
+    })
+
+    it('When filtering results by keywords, only exact matches to the post content are expected.', () => {
+        const key =  'encontrada';
+        cy.listPostByKey(key).then(res => {
+            expect(res.body[0].postContent).to.eq(posts.postContent);
+            expect(res.status).to.eq(200);
+        })
+    })
 })
