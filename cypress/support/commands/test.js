07a5cf4292ad94f94393afbf489baff8f287d1b4
Guilherme da Mata <gcordeiro773@gmail.com>
2025-04-13 19:33:51 -0300

    fix: excluding environment tests

--- cypress/support/commands/test.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('viewPosting', (take, skip) => {
-  cy.request({
-      method: 'GET',
-      url: `https://localhost:7240/api/posting/latest?take=${take}&skip=${skip}`,
-      failOnStatusCode: false,
-  })
-})
